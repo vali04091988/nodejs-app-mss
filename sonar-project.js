7e279c59960f55d75af2c2a39696724a5039a33c
vali04091988 <102497424+vali04091988@users.noreply.github.com>
2022-07-28 21:19:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://43.204.102.46:9000',
+  serverUrl: 'http://3.111.51.246:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '7c9a8d2db78f09d3e50599593bf676e6afccf613',
+	    'sonar.login': 'de83a47d60d4ca7e6268fc8a266645545321b715',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
